0147057e2be02916aae19cce6691747f77ee0d0f
Maxime FLAMENT <maximeflamentweb@gmail.com>
2023-10-27 14:00:08 +0200

    Update ProfileInfos.tsx with my new job

--- components/ProfileInfos.tsx
+++ components/ProfileInfos.tsx
@@ -2,7 +2,34 @@ import siteMetadata from '@/data/siteMetadata'
 
 const ProfileInfos = () => {
   return (
-    <div className="mb-4">
+    <div className="mb-4 space-y-2">
+      <div className="flex items-center text-gray-700 dark:text-gray-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1}
+            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+          />
+        </svg>
+        <span className="px-2">
+          Software Engineer{' '}
+          <a
+            href="https://digitalrecruiters.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          >
+            @DigitalRecruiters
+          </a>
+        </span>
+      </div>
       <div className="flex items-center text-gray-700 dark:text-gray-200">
         <svg
           xmlns="http://www.w3.org/2000/svg"
