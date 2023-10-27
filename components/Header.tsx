f440eff7e66b45ea638305f1f218ce7a82d0c449
Maxime FLAMENT <maximeflamentweb@gmail.com>
2023-10-27 14:40:16 +0200

    Change logo brightness

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
           <Link href="/" aria-label={siteMetadata.headerTitle}>
             <div className="flex items-center justify-between">
               <div className="mr-3">
-                <Logo />
+                <Logo className="brightness-150" />
               </div>
               {typeof siteMetadata.headerTitle === 'string' ? (
                 <div className="hidden h-6 text-2xl leading-5 font-semibold sm:block">
